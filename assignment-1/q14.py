9fc11c66b27fa07b8e60bf72304c53410848918e
alan <alangeorgemathews09@gmail.com>
2023-02-27 01:40:33 +0530

    Added more files

---
+++ assignment-1/q14.py
@@ -0,0 +1,17 @@
+a = float(input("Enter the length of side a: "))
+b = float(input("Enter the length of side b: "))
+c = float(input("Enter the length of side c: "))
+
+
+if a + b <= c or a + c <= b or b + c <= a:
+    print("Invalid triangle.")
+else:
+    
+    sides = [a, b, c]
+    sides.sort()
+
+    
+    if sides[0]*2 + sides[1]*2 == sides[2]*2:
+        print("The triangle is a right triangle.")
+    else:
+        print("The triangle is not a right triangle.")
