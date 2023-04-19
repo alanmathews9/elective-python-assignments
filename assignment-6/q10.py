46fba819a58310e0e4d56bd3777aa1f810458f5c
alan <alangeorgemathews09@gmail.com>
2023-04-20 01:59:42 +0530

    q10

---
+++ assignment-6/q10.py
@@ -0,0 +1,13 @@
+#fibonacci series
+
+def fib(n):
+    if n == 1:
+        return 0
+    elif n == 2:
+        return 1
+    else:
+        return fib(n-2) + fib(n-1)
+
+
+for i in range(1, 10):
+    print(fib(i))
