2e914661d77035770b70f33ac74d049f2b9318f6
alan <alangeorgemathews09@gmail.com>
2023-04-20 01:41:49 +0530

    q1

---
+++ assignment-6/q1.py
@@ -0,0 +1,9 @@
+# 1. Describe about modules and packages in Python. Illustrate withexamples, how modules are used in Python Programs.
+
+
+import math as m
+import math
+print("The value of pi is", math.pi)
+
+
+print("sqr root of 25 is", m.sqrt(25))
